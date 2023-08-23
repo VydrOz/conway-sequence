46dc2d6b27957742540d8afeefe1af511f0805a3
VydrOz <61025448+VydrOz@users.noreply.github.com>
2023-08-24 01:01:56 +0200

    rework global design

--- scripts/conway.js
+++ scripts/conway.js
@@ -1,59 +1,49 @@
 // elements
-let prevVal = document.getElementById('prev-val');
-let currVal = document.getElementById('curr-val');
-let counter = document.getElementById('count');
-let btnReset = document.getElementById('clear');
+let visualizer = document.getElementById('visualizer');
+let progressBar = document.getElementById('progess-bar');
+
 
 // variables
 let digits = [3];
 let nextDigits = "";
 
-window.addEventListener('load', async () => {
-    prevVal.innerHTML = digits[0];
-    currVal.innerHTML = nextDigits;
-    counter.innerHTML = 0;
+window.addEventListener('load', () => {
+    let d = document.createElement("div");
+    visualizer.appendChild(d);
+    d.innerHTML = `<span id='digit-${digits[0]}'>${digits[0]}</span>`
     loop();
 })
 
 async function loop() {
     for (var i = 0; i < Infinity; i++) {
-        await conway();
-        counter.innerHTML = i;
+        let d = document.createElement("div");
+        visualizer.appendChild(d);
+        updateProgressBar(0, 1);
+        await conway(d);
+        digits = nextDigits.match(/(.)\1*/g).map(Number);
+        nextDigits = "";
+        await sleep(150);
     }
 }
 
-async function conway() {
+async function conway(elem) {
     for (let i = 0; i < digits.length; i++) {
-        await sleep(250);
-        prevVal.innerHTML = digits.slice(0, i).join('')
-            + '<span style="border: solid red 0.5px;">' + digits[i] + '</span>' +  digits.slice(i+1, digits.length).join('');
-        await sleep(500);
         let num = digits[i].toString();
-        let res = num.length + num[0];
-        currVal.innerHTML = nextDigits + '<span style="border: solid red 0.5px;">' + res + '</span>';
-        nextDigits += res;
+        nextDigits += num.length + num[0];
+        elem.innerHTML += `<span id='digit-${num.length}'>${num.length}</span>`;
+        updateProgressBar(i-0.5, digits.length);
+        await sleep(75);
+        elem.innerHTML += `<span id='digit-${num[0]}'>${num[0]}</span>`
+        updateProgressBar(i, digits.length);
+        await sleep(75);
     }
-
-    digits = nextDigits.match(/(.)\1*/g).map(Number);
-    nextDigits = "";
-    prevVal.innerHTML = digits.join('');
-    currVal.innerHTML = nextDigits;
-
-    /*var conwayInterval = setInterval(() => {
-        if(digits.length > i) {
-            let num = digits[i].toString();
-            let res = num.length + num[0];
-            currVal.innerHTML += nextDigits + '<span style="color: red;">' + res + '</span>';
-            nextDigits += res;
-            if(digits.length < i++) {
-                clearInterval(conwayInterval);
-                callback();
-            }
-        }
-    }, 2000);*/
+    updateProgressBar(1, 1);
 }
 
+function updateProgressBar(val, max) {
+    progressBar.style.width = `${(val/max)*100}%`;
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
